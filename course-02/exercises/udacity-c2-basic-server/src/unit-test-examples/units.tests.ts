cbc74210e037974478b471a72495f87ca35e3491
Musoftware <mahmoudmn810@gmail.com>
2022-11-22 17:06:17 +0200

    my updates

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
@@ -1,39 +1,57 @@
-import { add, divide } from './units';
+import { add, divide, concat } from "./units";
 
-import { expect } from 'chai';
-import 'mocha';
+import { expect } from "chai";
+import "mocha";
 
-describe('add function', () => {
-
-  it('should add two and two', () => {
-    const result = add(2,2);
-    expect(result).to.equal(4);
+describe("add function", () => {
+  it("should add 3 and 5", () => {
+    const result = add(3, 5);
+    expect(result).to.equal(8);
   });
-
-  it('should add -2 and two', () => {
-    const result = add(-2,2);
+  it("should add -2 and 2", () => {
+    const result = add(-2, 2);
     expect(result).to.equal(0);
   });
-
 });
 
-describe('divide', () => {
-
-  it('should divide 6 by 3', () => {
-    const result = divide(6,3);
-    expect(result).to.equal(2);
+describe("divide function", () => {
+  it("should divide 3 and 5", () => {
+    const result = divide(15, 5);
+    expect(result).to.equal(3);
   });
 
-  it('should divide 5 and 2', () => {
-    const result = divide(5,2);
+  it("should divide 5 and 2", () => {
+    const result = divide(5, 2);
     expect(result).to.equal(2.5);
   });
 
-  it('should throw an error if div by zero', () => {
-    expect(()=>{ divide(5,0) }).to.throw('div by 0')
+  it("div by 0", () => {
+    expect(() => {
+      divide(4, 0);
+    }).to.throw("div by 0");
+  });
+});
+
+describe("concat", () => {
+  it("should concat he and llo", () => {
+    const result = concat("he", "llo");
+    expect(result).to.equal("hello");
+  });
+  it("should concat he and ", () => {
+    expect(() => {
+      concat("he", "");
+    }).to.throw("b is empty");
+  });
+
+  it("should concat and llo", () => {
+    expect(() => {
+      concat("", "llo");
+    }).to.throw("a is empty");
   });
 
 });
+
+
 
 // @TODO try creating a new describe block for the "concat" method
 // it should contain an it block for each it statement in the units.ts @TODO.
